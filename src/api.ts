f9803f841cb1086a58f949ae77a58453c118c0e4
Nl <naftalilouranny@gmail.com>
2025-08-21 22:08:24 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,16 @@
 import axios from 'axios'
-import { supabase } from './lib/supabaseClient' // Importe seu cliente Supabase
+import { supabase } from './lib/supabaseClient' 
 
-// A URL base da sua API do backend
 const api = axios.create({
   baseURL: 'http://localhost:3000',
 })
 
-// Interceptor que adiciona o token JWT do Supabase em cada requisição
+
 api.interceptors.request.use(
   async (config) => {
     const { data } = await supabase.auth.getSession()
 
-    // Se existir uma sessão ativa, adiciona o token de acesso ao cabeçalho Authorization
+    
     if (data.session?.access_token) {
       config.headers.Authorization = `Bearer ${data.session.access_token}`
     }
