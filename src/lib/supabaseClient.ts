94c4928cdbcabeb80bb0537f995ab789312925be
Nl <naftalilouranny@gmail.com>
2025-08-21 22:05:24 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Pega a URL e a chave do arquivo .env que criamos no passo anterior
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-// Cria a conexão/cliente do Supabase usando suas credenciais
-// e exporta para que possamos usá-lo em qualquer parte do nosso código
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
